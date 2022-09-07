6dd80a218d067b9bb52c1ed756ef7febac378b6a
PiousCrossten <112157030+PiousCrossten@users.noreply.github.com>
2022-09-07 21:38:17 +0530

    Multiplication Table
    It is a Simple Java code to Print a Multiplication Table Upto how times it is Required.

---
+++ Multitable.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Multitable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("This is the Program to Print Multiplication Table");
+      Scanner a=new Scanner(System.in);
+      System.out.println("Enter the Number for which You need a muliplication table");
+      int x = a.nextInt();
+      for(int i=1;i<=1000000;i++)
+      {
+    	  System.out.print(+x);
+    	  System.out.print( "*" );
+    	  System.out.print(+i);
+    	  System.out.print( "=" );
+    	  System.out.print(+x*i);
+    	  System.out.println( "");
+      }
+      
+	}
+
+}
